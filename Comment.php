84cbf790c36444da8a5b1d571d4439e1e8a74693
Bastian Peghaire <bastian.peghaire@gmail.com>
2014-05-24 16:56:17 +0200

    Add v1

---
+++ Comment.php
@@ -0,0 +1,65 @@
+<?php
+
+class Comment {
+    /**
+     * Get comments by parent id method
+     *
+     * @param Int $idParent
+     * @return Array
+     */
+    public function get_comments_by_parent_id($idParent) {
+        $root = simplexml_load_file('data/comments.xml');
+        $comments = [];
+        foreach($root->xpath("//comment") as $comment) {
+            if(intval($comment['idParent']) === $idParent){
+                array_push($comments, array((string)$comment['id'], (string) $comment->author, intval($comment->mark), (string) $comment->text ));
+                
+            } 
+        }
+        return $comments; 
+    }
+    
+    /**
+     * Add comment method
+     *
+     * @param String $id
+     * @param Int $idParent
+     * @param String $author
+     * @param Int $mark
+     * @param String $text
+     * @return String
+     */
+    public function add_comment($id, $idParent, $author, $mark, $text) {
+
+        $root = new DOMDocument();
+        $root->load('data/comments.xml');
+        $comments = $root->getElementsByTagName('comments')->item(0);
+
+        // Create new node with parameters
+        $comment = $root->createElement('comment');
+        $root->createElement('comment');
+        $authorElem = $root->createElement('author', $author);
+        $markElem = $root->createElement('mark', $mark);
+        $textElem = $root->createElement('text', $text);
+        $comment->setAttribute('idParent', $idParent);
+        $comment->setAttribute('id', $id);
+        $comment->appendChild($authorElem);
+        $comment->appendChild($markElem);
+        $comment->appendChild($textElem);
+
+        $comments->appendChild($comment);
+
+        // Validation of new element with schema
+        if($root->schemaValidate('data/schemaComments.xsd')) {
+
+            $root->save('data/comments.xml');
+        } else {
+
+            return "Error: input doesn't match xsd schema rules";
+        } 
+        return $root->saveXML($root);
+    }
+
+
+    
+}
---
+++ SoapServer.php
@@ -0,0 +1,33 @@
+<?php 
+
+require 'init_autoloader.php';
+
+require_once realpath('Comment.php');
+
+/*
+* Check to see if soap call is to be handled
+* or if the WSDL should be displayed
+*/
+if(isset($_GET['wsdl']))
+{
+    // Autogenerate wsdl file
+    $autodiscover = new Zend\Soap\AutoDiscover();
+    $autodiscover->setClass('Comment')
+                 ->setUri( 'http://' . $_SERVER["SERVER_NAME"] .
+    $_SERVER['PHP_SELF']);
+    echo $autodiscover->toXml();
+}
+else
+{
+
+    $wsdl = 'http://' . $_SERVER["SERVER_NAME"] .
+    $_SERVER['PHP_SELF'] . '?wsdl';
+    
+    // Create and set server
+
+    $server = new Zend\Soap\Server($wsdl);
+
+    $server->setClass('Comment');
+ 
+    $server->handle();
+}
